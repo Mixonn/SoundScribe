9127e7e91a4d3082f02a23d39563295825b0fcca
Osipiuk, Bartosz <bartosz.osipiuk@impaqgroup.com>
2019-10-21 19:15:58 +0200

    Init BE

---
+++ soundscribe-be/soundscribe-be-core/src/main/java/com/soundscribe/core/SondscribeBeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.soundscribe.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SondscribeBeCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SondscribeBeCoreApplication.class, args);
+  }
+
+}
---
+++ soundscribe-be/soundscribe-be-core/src/test/java/com/soundscribe/core/SondscribeBeCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundscribe.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SondscribeBeCoreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
