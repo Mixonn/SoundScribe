18bca7c1bd43f591678e555fc2b19ee74201d6bf
Bartosz Osipiuk <osipiuk.bartosz@gmail.com>
2019-10-27 10:59:23 +0100

    UI update

---
+++ soundscribe-ui/components/verovio/StaffLines.vue
@@ -0,0 +1,11 @@
+<template />
+
+<script>
+export default {
+  name: 'StaffLines'
+}
+</script>
+
+<style scoped>
+
+</style>
--- soundscribe-ui/pages/inspire.vue
+++ soundscribe-ui/pages/inspire.vue
@@ -14,6 +14,29 @@
           </small>
         </footer>
       </blockquote>
+      <div class="panel-body">
+        <div id="app" class="panel" style="border: 1px solid lightgray; min-height: 800px;" />
+      </div>
+      <script type="module">
+        import 'verovio';
+
+        const options = {
+        defaultView: 'document', // instead of 'responsive' by default
+        defaultZoom: 4 // 0-7, default is 3
+        }
+
+        // Create the app - here with an empty option object
+        const app = new Verovio.App(document.getElementById("app"), {options});
+
+        // Load a file (MEI or MusicXML)
+        fetch("https://raw.githubusercontent.com/music-encoding/sample-encodings/master/MEI_4.0/Music/Complete_examples/Bach-J-C_Fughette_Gmaj.mei")
+        .then(function(response) {
+        return response.text();
+        })
+        .then(function(text) {
+        app.loadData(text);
+        });
+      </script>
     </v-flex>
   </v-layout>
 </template>
