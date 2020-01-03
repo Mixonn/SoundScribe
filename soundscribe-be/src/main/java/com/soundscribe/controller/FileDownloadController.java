37f2af428620c4b9c3de5cb7b1ff7e6afc46b232
Mateusz Lamecki <lamecki.mateusz@gmail.com>
2020-01-03 02:54:35 +0100

    Add list-files controller and disable CORS

--- soundscribe-be/src/main/java/com/soundscribe/controller/FileDownloadController.java
+++ soundscribe-be/src/main/java/com/soundscribe/controller/FileDownloadController.java
@@ -1,17 +1,22 @@
 package com.soundscribe.controller;
 
+import com.soundscribe.storage.StorageService;
 import com.soundscribe.utilities.SoundscribeConfiguration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,12 +24,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class FileDownloadController {
   private final SoundscribeConfiguration soundscribeConfiguration;
+  private final StorageService storageService;
+
+  @GetMapping("/list-files")
+  public List<String> listFiles(String extension) {
+    return storageService
+            .loadAll()
+            .map(x -> x.getFileName().toString())
+            .filter(x -> x.endsWith(extension))
+            .collect(Collectors.toList());
+  }
 
   @RequestMapping("/{filename}")
   public void downloadFile(
-      HttpServletRequest request,
-      HttpServletResponse response,
-      @PathVariable("filename") String filename) {
+          HttpServletRequest request,
+          HttpServletResponse response,
+          @PathVariable("filename") String filename) {
     Path file = Paths.get(soundscribeConfiguration.getSongDataStorage(), filename);
     if (Files.exists(file)) {
       response.setContentType("application/pdf");
